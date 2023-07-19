a857c78ef57f40f8bf157ad685b9cd2f32d2ed35
GCC ❌ CSD <105968334+gcc-x-csd@users.noreply.github.com>
2023-07-19 23:00:12 +0530

    Delete Solutions directory

--- Solutions/Q-01/sum.pl
+++
@@ -1,8 +0,0 @@
-% By - Kaustav Purkayastha ( Gurucharan University, Silchar - @ Department of Computer Science )
-
-% 1. Write a Prolog program to calculate the sum of two numbers.
-
-
-% Define a rule named `sum` that takes two arguments, `X` and `Y`, and calculates their sum
-sum(X, Y, Sum) :-
-  Sum is X + Y. % Calculate the sum of `X` and `Y` and store it in the variable `Sum`
